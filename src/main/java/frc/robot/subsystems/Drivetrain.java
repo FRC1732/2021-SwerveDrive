b68366d351ecd50575591c9ce262be0c3a8e568a
Alan Bork <aebork@gmail.com>
2021-03-23 20:15:25 -0500

    Added the Gyro to Drivetrain

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -4,8 +4,11 @@
 
 package frc.robot.subsystems;
 
-import edu.wpi.first.wpilibj.AnalogGyro;
+import com.kauailabs.navx.frc.AHRS;
+import edu.wpi.first.wpilibj.SPI;
+import edu.wpi.first.wpilibj.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.geometry.Translation2d;
+import edu.wpi.first.wpilibj.interfaces.Gyro;
 import edu.wpi.first.wpilibj.kinematics.ChassisSpeeds;
 import edu.wpi.first.wpilibj.kinematics.SwerveDriveKinematics;
 import edu.wpi.first.wpilibj.kinematics.SwerveDriveOdometry;
@@ -21,51 +24,58 @@ public class Drivetrain extends SubsystemBase {
   public static final double kMaxSpeed = 3.0; // 3 meters per second
   public static final double kMaxAngularSpeed = Math.PI; // 1/2 rotation per second
 
-  private final Translation2d m_frontLeftLocation = new Translation2d(0.193, 0.193);
-  private final Translation2d m_frontRightLocation = new Translation2d(0.193, -0.193);
-  private final Translation2d m_backLeftLocation = new Translation2d(-0.193, 0.193);
-  private final Translation2d m_backRightLocation = new Translation2d(-0.193, -0.193);
+  private final Translation2d m_frontLeftLocation = new Translation2d(TRACKWIDTH / 2.0, WHEELBASE / 2.0);
+  // Translation2d(0.193, 0.193);
+  private final Translation2d m_frontRightLocation = new Translation2d(TRACKWIDTH / 2.0, -WHEELBASE / 2.0);
+  // Translation2d(0.193, -0.193);
+  private final Translation2d m_backLeftLocation = new Translation2d(-TRACKWIDTH / 2.0, WHEELBASE / 2.0);
+  // Translation2d(-0.193, 0.193);
+  private final Translation2d m_backRightLocation = new Translation2d(-TRACKWIDTH / 2.0, -WHEELBASE / 2.0);
+  // Translation2d(-0.193, -0.193);
 
-  private final SwerveModule m_frontLeft = new SwerveModule(Constants.DRIVETRAIN_M1_DRIVE, Constants.DRIVETRAIN_M1_AZIMUTH);
-  private final SwerveModule m_frontRight = new SwerveModule(Constants.DRIVETRAIN_M2_DRIVE, Constants.DRIVETRAIN_M2_AZIMUTH);
-  private final SwerveModule m_backLeft = new SwerveModule(Constants.DRIVETRAIN_M3_DRIVE, Constants.DRIVETRAIN_M3_AZIMUTH);
-  private final SwerveModule m_backRight = new SwerveModule(Constants.DRIVETRAIN_M4_DRIVE, Constants.DRIVETRAIN_M4_AZIMUTH);
+  private final SwerveModule m_frontLeft = new SwerveModule(Constants.DRIVETRAIN_M1_DRIVE,
+      Constants.DRIVETRAIN_M1_AZIMUTH);
+  private final SwerveModule m_frontRight = new SwerveModule(Constants.DRIVETRAIN_M2_DRIVE,
+      Constants.DRIVETRAIN_M2_AZIMUTH);
+  private final SwerveModule m_backLeft = new SwerveModule(Constants.DRIVETRAIN_M3_DRIVE,
+      Constants.DRIVETRAIN_M3_AZIMUTH);
+  private final SwerveModule m_backRight = new SwerveModule(Constants.DRIVETRAIN_M4_DRIVE,
+      Constants.DRIVETRAIN_M4_AZIMUTH);
 
-  private final AnalogGyro m_gyro = new AnalogGyro(0);
+  private final Gyro m_gyro;
 
-  private final SwerveDriveKinematics kinematics = new SwerveDriveKinematics(
-    new Translation2d(TRACKWIDTH / 2.0, WHEELBASE / 2.0),
-    new Translation2d(TRACKWIDTH / 2.0, -WHEELBASE / 2.0),
-    new Translation2d(-TRACKWIDTH / 2.0, WHEELBASE / 2.0),
-    new Translation2d(-TRACKWIDTH / 2.0, -WHEELBASE / 2.0)
-);
+  /*
+   * private final SwerveDriveKinematics kinematics = new SwerveDriveKinematics(
+   * new Translation2d(TRACKWIDTH / 2.0, WHEELBASE / 2.0), new
+   * Translation2d(TRACKWIDTH / 2.0, -WHEELBASE / 2.0), new
+   * Translation2d(-TRACKWIDTH / 2.0, WHEELBASE / 2.0), new
+   * Translation2d(-TRACKWIDTH / 2.0, -WHEELBASE / 2.0) );
+   */
 
-  private final SwerveDriveKinematics m_kinematics =
-      new SwerveDriveKinematics(
-          m_frontLeftLocation, m_frontRightLocation, m_backLeftLocation, m_backRightLocation);
+  private final SwerveDriveKinematics m_kinematics = new SwerveDriveKinematics(m_frontLeftLocation,
+      m_frontRightLocation, m_backLeftLocation, m_backRightLocation);
 
-  private final SwerveDriveOdometry m_odometry =
-      new SwerveDriveOdometry(m_kinematics, m_gyro.getRotation2d());
+  private final SwerveDriveOdometry m_odometry = new SwerveDriveOdometry(m_kinematics, new Rotation2d());
 
   public Drivetrain() {
+    m_gyro = new AHRS(SPI.Port.kMXP);
     m_gyro.reset();
   }
 
   /**
    * Method to drive the robot using joystick info.
    *
-   * @param xSpeed Speed of the robot in the x direction (forward).
-   * @param ySpeed Speed of the robot in the y direction (sideways).
-   * @param rot Angular rate of the robot.
-   * @param fieldRelative Whether the provided x and y speeds are relative to the field.
+   * @param xSpeed        Speed of the robot in the x direction (forward).
+   * @param ySpeed        Speed of the robot in the y direction (sideways).
+   * @param rot           Angular rate of the robot.
+   * @param fieldRelative Whether the provided x and y speeds are relative to the
+   *                      field.
    */
   @SuppressWarnings("ParameterName")
   public void drive(double xSpeed, double ySpeed, double rot, boolean fieldRelative) {
-    var swerveModuleStates =
-        m_kinematics.toSwerveModuleStates(
-            fieldRelative
-                ? ChassisSpeeds.fromFieldRelativeSpeeds(xSpeed, ySpeed, rot, m_gyro.getRotation2d())
-                : new ChassisSpeeds(xSpeed, ySpeed, rot));
+    var swerveModuleStates = m_kinematics.toSwerveModuleStates(
+        fieldRelative ? ChassisSpeeds.fromFieldRelativeSpeeds(xSpeed, ySpeed, rot, m_gyro.getRotation2d())
+            : new ChassisSpeeds(xSpeed, ySpeed, rot));
     SwerveDriveKinematics.normalizeWheelSpeeds(swerveModuleStates, kMaxSpeed);
     m_frontLeft.setDesiredState(swerveModuleStates[0]);
     m_frontRight.setDesiredState(swerveModuleStates[1]);
@@ -75,16 +85,12 @@ public class Drivetrain extends SubsystemBase {
 
   /** Updates the field relative position of the robot. */
   public void updateOdometry() {
-    m_odometry.update(
-        m_gyro.getRotation2d(),
-        m_frontLeft.getState(),
-        m_frontRight.getState(),
-        m_backLeft.getState(),
+    m_odometry.update(m_gyro.getRotation2d(), m_frontLeft.getState(), m_frontRight.getState(), m_backLeft.getState(),
         m_backRight.getState());
   }
 
-    // Stops the motors from 
-    public void stop() {
-      drive(0,0,0,false);
-    }
+  // Stops the motors from
+  public void stop() {
+    drive(0, 0, 0, false);
+  }
 }
