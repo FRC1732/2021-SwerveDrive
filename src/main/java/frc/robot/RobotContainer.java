5023727d0270f06f53b9a6dfc541790c5b8649e3
Ryan Guinn <Ryan.Guinn@cognex.com>
2021-06-25 23:10:48 -0500

    end of warehouse 6-24

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -5,6 +5,8 @@ import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
 import edu.wpi.first.wpilibj2.command.button.Button;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
@@ -205,7 +207,24 @@ public class RobotContainer {
    */
   public Command getAutonomousCommand() {
     // return (Command) autonomousModeOption.getSelected();
-    return new DriveAndShootAuto(s_drivetrain, indexer, intake, feeder, shooter);
+    //return new DriveAndShootAuto(s_drivetrain, indexer, intake, feeder, shooter);
+    return new InstantCommand(() -> intake.takeIn(true), intake)
+    .andThen(new WaitCommand(0.5))
+    .andThen(new InstantCommand(() -> intake.takeIn(false), intake))
+    .andThen(new InstantCommand(() -> s_drivetrain.drive(-0.5d, 0d, 0.04d, false), s_drivetrain))
+    .andThen(new WaitCommand(3.5d))
+    .andThen(new InstantCommand(() -> s_drivetrain.stop(), s_drivetrain))
+    .andThen(new InstantCommand(() -> shooter.maintainRPM(), shooter))
+    .andThen(new WaitCommand(3d))
+    .andThen(new FeedBallToShooter(indexer, intake, feeder));
+  }
+  
+  public void initTeleop(){
+    s_drivetrain.stop();
+    indexer.stop();
+    intake.takeIn(false);
+    feeder.stop();
+    shooter.stopMotors();
   }
 
   public void setStartPosition() {
--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -19,7 +19,7 @@ public class Shooter extends SubsystemBase {
 
   private TalonFX motorOne;
   private TalonFX motorTwo;
-  private static final double SETPOINT = 7800d;
+  private static final double SETPOINT = 8000;
   private static final int DEADBAND = 500;
   private boolean setpoint = false;
 
@@ -45,11 +45,11 @@ public class Shooter extends SubsystemBase {
   public void periodic() {
     if(setpoint){
     if(motorOne.getSelectedSensorVelocity() < SETPOINT){
-      motorOne.set(ControlMode.PercentOutput, 0.71d);
-      motorTwo.set(ControlMode.PercentOutput, 0.71d);
+      motorOne.set(ControlMode.PercentOutput, 0.5d);
+      motorTwo.set(ControlMode.PercentOutput, 0.5d);
     } else {
-      motorOne.set(ControlMode.PercentOutput, 0.46d);
-      motorTwo.set(ControlMode.PercentOutput, 0.46d);
+      motorOne.set(ControlMode.PercentOutput, 0.4d);
+      motorTwo.set(ControlMode.PercentOutput, 0.4d);
     }
     // This method will be called once per scheduler 
     if (motorOne.getSelectedSensorVelocity() > 100){
